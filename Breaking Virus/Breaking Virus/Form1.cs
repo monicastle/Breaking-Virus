fa40d2d60e98c4448ece4d86862db9ea193bb519
monicastle <monicavaleriamendoza@yahoo.com>
2023-02-17 14:52:12 -0600

    Server y Client

--- Breaking Virus/Breaking Virus/Form1.cs
+++ Breaking Virus/Breaking Virus/Form1.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
 namespace Breaking_Virus
 {
     public partial class Form1 : Form
@@ -5,6 +14,39 @@ namespace Breaking_Virus
         public Form1()
         {
             InitializeComponent();
+
+            // INICIO
+            try
+            {
+                string SERVER_IP = "192.168.68.106";
+                int PORT_NO = 8001;
+                while (true)
+                {
+                    TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+                    Console.WriteLine("Connecting.....");
+                    NetworkStream nwStream = client.GetStream();
+                    Console.WriteLine("Connected");
+                    Console.Write("Enter the string to be transmitted : ");
+                    String textToSend = Console.ReadLine();
+                    byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                    //---send the text---
+                    Console.WriteLine("Sending : " + textToSend);
+                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                    //---read back the text---
+                    byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                    int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                    Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                    Console.ReadLine();
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            // FINAL
         }
+
     }
 }
